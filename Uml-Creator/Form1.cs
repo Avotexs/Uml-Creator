a262daa65e563fe6d800f4baede890abdcb706cc
TAHA <TAHA@DESKTOP-93VVBV0>
2024-11-06 12:08:29 +0100

    taha F commit

--- Uml-Creator/Form1.cs
+++ Uml-Creator/Form1.cs
@@ -16,5 +16,12 @@ namespace Uml_Creator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MessageBox.Show("taha");
+            ////
+        }
     }
 }
